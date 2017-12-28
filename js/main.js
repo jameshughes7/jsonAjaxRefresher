ac0350eacd647397fbaa1980ddedca9bf4fc600f
James Hughes <hugjimbo77@gmail.com>
2017-12-28 14:14:37 +0000

    added css style and pageCounter variable

--- js/main.js
+++ js/main.js
@@ -1,12 +1,28 @@
+var pageCounter = 1;
+
+var animalContainer = document.getElementById("animal-info");
+
 var btn = document.getElementById("btn");
 
 btn.addEventListener("click", function() {
   var xhttp = new XMLHttpRequest();
-  xhttp.open('GET','https://learnwebcode.github.io/json-example/animals-1.json');
+  xhttp.open('GET','https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
 
   xhttp.onload = function() {
     var data = JSON.parse(xhttp.responseText);
-    console.log(data[0]);
+    renderHTML(data);
   };
   xhttp.send();
-})
+  pageCounter ++;
+  if (pageCounter > 3) {
+    btn.classList.add("hide-me")
+  }
+});
+
+function renderHTML(data) {
+  var htmlString = "";
+  for (i = 0; i < data.length; i++) {
+    htmlString += "<p>" + data[i].name + " is a " + data[i].species + ".</p>";
+  }
+  animalContainer.insertAdjacentHTML('beforeend',htmlString);
+}
