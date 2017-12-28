a67f7d2673f8b93a965310f27ff6a086c255b8e6
James Hughes <hugjimbo77@gmail.com>
2017-12-28 13:50:23 +0000

    pulled in data using AJAX

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var xhttp = new XMLHttpRequest();
 xhttp.open('GET','https://learnwebcode.github.io/json-example/animals-1.json');
 
 xhttp.onload = function() {
-  console.log(xhttp.responseText);
+  var data = JSON.parse(xhttp.responseText);
+  console.log(data[0]);
 };
 xhttp.send();
